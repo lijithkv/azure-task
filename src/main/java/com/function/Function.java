f52e99eb02a610d04e9d00f9bfba9aa3ee9bba85
lijithkv <80882821+lijithkv@users.noreply.github.com>
2021-03-18 20:30:02 +0530

    Update Function.java

--- src/main/java/com/function/Function.java
+++ src/main/java/com/function/Function.java
@@ -16,11 +16,7 @@ import java.util.Optional;
  * Azure Functions with HTTP Trigger.
  */
 public class Function {
-    /**
-     * This function listens at endpoint "/api/HttpExample". Two ways to invoke it using "curl" command in bash:
-     * 1. curl -d "HTTP Body" {your host}/api/HttpExample
-     * 2. curl "{your host}/api/HttpExample?name=HTTP%20Query"
-     */
+  
     @FunctionName("HttpExample")
     public HttpResponseMessage run(
             @HttpTrigger(
